4ffe8a6c1ca21779e9eaedfeefaedb35493e5964
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-02-18 23:29:35 -0400

    feat: agrega saludo basica con hola

--- src/saludador.js
+++ src/saludador.js
@@ -1,9 +1,6 @@
 function saludar() {
   document.getElementById("resultado").textContent = "Hola";
+  
 }
 
-function saludar() {
-  const nombre = document.getElementById("nombre").ariaValueMax;
-  document.getElementById("resultado").textContent = "Hola " + nombre;
-}
 
