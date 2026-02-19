09674008278252e5338f8b60fcd7622a2a22b51c
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-02-18 23:47:48 -0400

    feat: agrega saludo según la hora del día

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,17 @@
 function saludar() {
   const nombre = document.getElementById("nombre").value;
-  document.getElementById("resultado").textContent = "Hola";
+  const hora = new Date().getHours();
+  let saludo = "";
+
+  if(hora < 12){
+    saludo = "Buenos dias";
+  } else if(hora < 18){
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+  document.getElementById("resultado").textContent = saludo + " " + nombre;
+
 }
 
 
