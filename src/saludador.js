6fd3b901cd69f4a2fb51630a2b6c80796f89d30b
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-02-18 23:17:15 -0400

    feat: agrega saludo personalizado por nombre

--- src/saludador.js
+++ src/saludador.js
@@ -2,4 +2,8 @@ function saludar() {
   document.getElementById("resultado").textContent = "Hola";
 }
 
+function saludar() {
+  const nombre = document.getElementById("nombre").ariaValueMax;
+  document.getElementById("resultado").textContent = "Hola " + nombre;
+}
 
