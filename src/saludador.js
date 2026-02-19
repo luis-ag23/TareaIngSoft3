986fa12f1c4dd40eebad92e90105a0d19a97f4a7
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-02-18 23:54:00 -0400

    feat: agrega saludo por género

--- src/saludador.js
+++ src/saludador.js
@@ -1,7 +1,9 @@
 function saludar() {
   const nombre = document.getElementById("nombre").value;
+  const genero = document.getElementById("genero").value;
   const hora = new Date().getHours();
   let saludo = "";
+  let titulo_de_cortesia = "";
 
   if(hora < 12){
     saludo = "Buenos dias";
@@ -10,7 +12,10 @@ function saludar() {
   } else {
     saludo = "Buenas noches";
   }
-  document.getElementById("resultado").textContent = saludo + " " + nombre;
+
+  if(genero === "m") titulo_de_cortesia = "Sr.";
+  else titulo_de_cortesia = "Sra."
+  document.getElementById("resultado").textContent = saludo + " " + titulo + " " + nombre;
 
 }
 
